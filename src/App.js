b21e7c6d44ecd6607ee95056723cb86373a1d29c
sabrinaZait <zsabrina941@outlook.fr>
2022-06-01 19:21:39 +0200

    jsx commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import img from './img.jpg';
+import myVideo from "./myVideo.mp4";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+
+        <h1 class="title red">ZAIT Sabrina</h1>
+        <h2 class="title red">Welcome to Diagonalley !!</h2>
+        <button>Buy your sorcer's fournitures </button>
+        <br />
+        <img src={img} />
+     </div>
+
+     
+     <video width="320" height="240" controls>
+        <source src={myVideo} type="video/mp4" />
+      </video>
     </div>
-  );
+          );
 }
 
-export default App;
+          export default App;
